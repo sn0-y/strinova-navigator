a00e574c28802221d9b9e97ccd48b50a4d5d9486
Snowy <Showhanur@hotmail.com>
2026-02-17 19:13:25 +0400

    refactor: Make config public

--- src/config.template.ts
+++
@@ -1,8 +0,0 @@
-// Server Specific Configurations
-export const config = {
-    prefix: '',
-    roles: {
-        staff: '',
-        leadmod: '',
-    }
-}
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Server Specific Configurations
+export const config = {
+    prefix: '&',
+    roles: {
+        staff: '1182953334492106772',
+        leadmod: '1182953730572820562',
+    }
+}
