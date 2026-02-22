a3ab31c93cdbd1bfa5ec8e438ec4c54c3eb1d706
Snowy <Showhanur@hotmail.com>
2026-02-22 09:14:16 +0400

    refactor: restructure config based on environment

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,17 @@
 // Server Specific Configurations
-export const config = {
+
+interface config {
+	prefix: string;
+	roles: {
+		staff: string;
+		leadmod: string;
+	};
+	channels: {
+		eventNotifications: string;
+	};
+}
+
+const production: config = {
 	prefix: '&',
 	roles: {
 		staff: '1182953334492106772',
@@ -9,3 +21,18 @@ export const config = {
 		eventNotifications: '1307316881492938822'
 	}
 };
+
+const development: config = {
+	prefix: '&',
+	roles: {
+		staff: '1182953334492106772',
+		leadmod: '1182953730572820562'
+	},
+	channels: {
+		eventNotifications: '1307316881492938822'
+	}
+};
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const config: config = isProduction ? production : development;
